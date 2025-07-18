193afcbbdc8b496ecc0199cfa64c741b3c392616
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2025-07-18 21:08:39 +0200

    Add application of default values
    Add application of default values for optional fields in dag json parser
    
    Signed-off-by: Nikita-Nick Funk <nikita.nick.funk@gmail.com>

--- internal/dag/parser.go
+++ internal/dag/parser.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"time"
 )
 
 type Parser interface {
@@ -29,5 +30,49 @@ func (p *JSONParser) Parse(reader io.Reader) (*Dag, error) {
 		return nil, fmt.Errorf("invalid dag config: %w", err)
 	}
 
+	p.applyDefaults(&dag)
+
 	return &dag, nil
+}
+
+func (p *JSONParser) applyDefaults(dag *Dag) {
+	const (
+		defaultRetries    = 0
+		defaultRetryDelay = 5 * time.Second
+		defaultTimeout    = 60 * time.Second
+	)
+
+	for i := range dag.Tasks {
+		task := &dag.Tasks[i]
+
+		if task.Retries == nil {
+			if dag.DefaultArgs != nil && dag.DefaultArgs.Retries != nil {
+				retriesCopy := *dag.DefaultArgs.Retries
+				task.Retries = &retriesCopy
+			} else {
+				task.Retries = new(int)
+				*task.Retries = defaultRetries
+			}
+		}
+
+		if task.RetryDelay == nil {
+			if dag.DefaultArgs != nil && dag.DefaultArgs.RetryDelay != nil {
+				delayCopy := *dag.DefaultArgs.RetryDelay
+				task.RetryDelay = &delayCopy
+			} else {
+				task.RetryDelay = new(TaskDuration)
+				*task.RetryDelay = TaskDuration(defaultRetryDelay)
+			}
+		}
+
+		if task.Timeout == nil {
+			if dag.DefaultArgs != nil && dag.DefaultArgs.TimeOut != nil {
+				timeoutCopy := *dag.DefaultArgs.TimeOut
+				task.Timeout = &timeoutCopy
+			} else {
+				task.Timeout = new(TaskDuration)
+				*task.Timeout = TaskDuration(defaultTimeout)
+			}
+		}
+	}
 }
